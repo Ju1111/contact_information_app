6abea236d9ae398261cf28ecb9c55c13072c821e
Ju111 <SchneiderJu@gmx.net>
2018-02-17 21:56:37 +0100

    method selects contacts with phone number including +1

--- main.rb
+++ main.rb
@@ -5,15 +5,26 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
+@contacts = read_contacts
+
 def display_contacts
-  contacts = read_contacts
-  contacts.map {|contact|
+  @contacts.map {|contact|
     contact[contact[:name]] = contact[:phone];
     contact.delete(:email);
     contact.delete(:name);
     contact.delete(:phone);
   }
-  puts contacts
+  puts @contacts
+end
+
+def select_contacts
+  selection = @contacts.select { |contact|
+      contact.any? { |key, value|
+        value.include?('+1')
+      }
+     }
+  puts selection
 end
 
 display_contacts
+select_contacts
