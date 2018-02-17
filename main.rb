74497e63b979bb19bd81bee55715441594464f73
Ju111 <SchneiderJu@gmx.net>
2018-02-17 17:16:45 +0100

    method deletes email key-value pair

--- main.rb
+++ main.rb
@@ -4,3 +4,13 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+def display_contacts
+  contacts = read_contacts
+  contacts.map {|contact|
+    contact.delete(:email)
+  }
+  puts contacts
+end
+
+display_contacts
