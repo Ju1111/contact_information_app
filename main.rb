7356a7831a337be99ad4b2e8ec9748acd5932ae3
Ju111 <SchneiderJu@gmx.net>
2018-02-18 14:29:48 +0100

    comments added

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
+# returns all contacts in an array of hashes with individual name as key and phonenumber as value
 def display_contacts
   contacts = read_contacts
   contacts.map {|contact|
@@ -16,6 +17,7 @@ def display_contacts
   contacts
 end
 
+# returns contacts with a phonenumber that includes "+1"
 def select_phone
   contacts = read_contacts
   phone_selection = contacts.select { |contact|
@@ -26,6 +28,7 @@ def select_phone
   phone_selection
 end
 
+# returns contacts with an email that contains ".org"
 def select_email
   contacts = read_contacts
   email_selection = contacts.select { |contact|
@@ -36,6 +39,7 @@ def select_email
   email_selection
 end
 
+# returns the amount of contacts that have a phonenumber which includes "+1"
 def count_phone
   phone_count = select_phone
   count = phone_count.reduce(0) { |sum, contact|
@@ -44,6 +48,7 @@ def count_phone
   count
 end
 
+# returns the amount of contacts that have an email which includes ".org"
 def count_email
   email_count = select_email
   count = email_count.reduce(0) { |sum, contact|
