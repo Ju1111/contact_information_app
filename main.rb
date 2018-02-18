4e3302996ab08745bcf3fd67c7588de363b8f5db
Ju111 <SchneiderJu@gmx.net>
2018-02-18 12:06:33 +0100

    instance variable @contacts changed to local variable

--- main.rb
+++ main.rb
@@ -5,29 +5,30 @@ def read_contacts
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
 
-@contacts = read_contacts
-
 def display_contacts
-  @contacts.map {|contact|
+  contacts = read_contacts
+  contacts.map {|contact|
     contact[contact[:name]] = contact[:phone];
     contact.delete(:email);
     contact.delete(:name);
     contact.delete(:phone);
   }
-  puts @contacts
+  contacts
 end
 
 def select_phone
-  phone_selection = @contacts.select { |contact|
+  contacts = read_contacts
+  phone_selection = contacts.select { |contact|
     contact.any? { |key, value|
       value.include?('+1')
     }
   }
-  return phone_selection
+  phone_selection
 end
 
 def select_email
-  email_selection = @contacts.select { |contact|
+  contacts = read_contacts
+  email_selection = contacts.select { |contact|
     contact.any? { |key, value|
       value.include?('.org')
     }
@@ -36,14 +37,14 @@ def select_email
 end
 
 def count_phone
-    phone_count = select_phone
-    count = phone_count.reduce(0) { |sum, contact|
+  phone_count = select_phone
+  count = phone_count.reduce(0) { |sum, contact|
     sum += 1
   }
-  return count
+  count
 end
 
-#display_contacts
-#puts select_phone
-#puts select_email
+puts display_contacts
+puts select_phone
+puts select_email
 puts count_phone
